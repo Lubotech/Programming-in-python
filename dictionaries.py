bd1fb504ac2e61992ea5eab365f1d6861ac5f049
Lubotech <luboyerajoel573@gmail.com>
2025-02-04 15:09:40 +0300

    Worked on lists

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+students = {
+  "Hermione": "Gryffindor",
+  "Harry": "Gryffindor",
+  "Ron": "Gryffindor",
+  "Hermione Granger": "Gryffindor",
+  "Draco Malfoy": "Slytherin",
+  "Voldemort": "Slytherin",
+            }
+
+for student in students:
+  print(f"{student}: {students[student]}")
