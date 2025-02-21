21c56fd9f56685c94c5df747bcc3bc8e622b40f5
Lubotech <luboyerajoel573@gmail.com>
2025-02-21 06:51:14 +0300

    Beginning exceptions

--- mario2.py
+++ mario2.py
@@ -1,18 +1,60 @@
-def main():
-  print_square(3)
+# def main():
+#   print_square(3)
   
-def print_square(size):
-  # For each row in square
-  for i in range(size):
+# def print_square(size):
+#   # For each row in square
+#   for i in range(size):
     
-    # For each brick in row
-    for j in range(size):
+#     # For each brick in row
+#     for j in range(size):
       
-      # Print a brick 
-        print("#", end="")
+#       # Print a brick 
+#         print("#", end="")
         
           
-    print()
+#     print()
 
   
+# main()  
+
+# def main():
+#   print_square(3)
+  
+# def print_square(size):
+#   # For each row in square
+#   for i in range(size):
+#     # Print the top and bottom borders
+#     print("#" * size)
+    
+#     # Print the middle bricks
+#     for _ in range(size - 2):
+#       print("#", end="")
+#       print(" " * (size - 2), end="")
+#       print("#")
+      
+# main()
+
+def main():
+  print_triangle(4)
+  
+def print_triangle(size):
+  for i in range(size):
+    print(" " * (size - 1 - i) + "#" * (2 * i + 1))
+    
 main()  
+
+print("")    
+
+
+def main1():
+   print_square(4)
+   
+def print_square(size):
+  for i in range(size):
+    print_row(size)
+    
+def print_row(width):
+  print("?" * width)
+  
+main1()         
+   
